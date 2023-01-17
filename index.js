99480bee3187f96b7b48dd299095183b730a8a19
Demarcus <demarcusprophet@gmail.com>
2023-01-17 08:57:24 -0600

    Functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+let distanceFromHqInBlocks = streetNum => {
+    return (streetNum < 42) ?  42 - streetNum : streetNum - 42;
+}
+console.log(distanceFromHqInBlocks(6))
+
+
+  function distanceFromHqInFeet(streetNum) {
+    return distanceFromHqInBlocks(streetNum) * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(distanceFromHqInFeet(start) - distanceFromHqInFeet(destination));
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance > 2500) {
+      return 'cannot travel that far';
+    } else if (distance <= 400) {
+      return 0;
+    } else if (distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else {
+      return 25;
+    }
+  }
+  
